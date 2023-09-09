ee2a9bc101a5ac6d2abacc45e7aa7c79716c6320
Josh Fung <joshuagyfung@gmail.com>
2023-09-09 13:00:24 -0700

    Setup foundation and fonts

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<h1 class="name">Joshua Fung</h1>
+<ul class="info-list">
+	<li class="info-item">Email</li>
+	<li class="info-item">Github</li>
+	<li class="info-item">Resume</li>
+	<li class="info-item">LinkedIn</li>
+</ul>
+
+<style>
+	.name {
+	}
+
+	.info-list {
+	}
+
+	.info-item {
+	}
+</style>
