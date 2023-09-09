03fd53a46a50dbe50ce22ae2e18a0f0f376f6a65
Josh Fung <joshuagyfung@gmail.com>
2023-09-09 16:53:00 -0700

    Added skeleton visuals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,47 @@
 <script lang="ts">
+	interface LinkItem {
+		text: string;
+		clickHandler: () => void;
+	}
+
+	const handleEmailClick = () => {
+		console.log('Clicked email');
+	};
+
+	const handleGithubClick = () => {
+		console.log('Clicked github');
+	};
+
+	const handleResumeClick = () => {
+		console.log('Clicked resume');
+	};
+
+	const handleLinkedInClick = () => {
+		console.log('Clicked linkedin');
+	};
+
+	let items: LinkItem[] = [
+		{ text: 'Email', clickHandler: handleEmailClick },
+		{ text: 'Github', clickHandler: handleGithubClick },
+		{ text: 'Resume', clickHandler: handleResumeClick },
+		{ text: 'LinkedIn', clickHandler: handleLinkedInClick }
+	];
 </script>
 
-<h1 class="name">Joshua Fung</h1>
-<ul class="info-list">
-	<li class="info-item">Email</li>
-	<li class="info-item">Github</li>
-	<li class="info-item">Resume</li>
-	<li class="info-item">LinkedIn</li>
-</ul>
+<div class="w-screen h-screen p-3 bg-rich-black">
+	<div class="mb-6">
+		<h1 class="text-8xl text-ivory font-stardom">Joshua</h1>
+		<h1 class="text-8xl text-ivory font-stardom">Fung</h1>
+	</div>
+
+	<div class="flex flex-col">
+		{#each items as item (item.text)}
+			<button on:click={item.clickHandler} class="text-2xl text-isabelline font-switzer text-left">
+				{item.text}
+			</button>
+		{/each}
+	</div>
+</div>
 
 <style>
-	.name {
-	}
-
-	.info-list {
-	}
-
-	.info-item {
-	}
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,24 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				stardom: ['Stardom', 'serif'],
+				switzer: ['Switzer var', 'sans-serif']
+			},
+			colors: {
+				'raisin-black': '#252323',
+				'slate-grey': '#70798C',
+				isabelline: '#F5F1ED',
+				ivory: '#FFFDED',
+				'yellow-isabelline': '#FFFFE3',
+				bone: '#DAD2BC',
+				khaki: '#A99985',
+				'rich-black': '#0D1B2A',
+				'oxford-blue': '#1B263B',
+				'yinmn-blue': '#415A77'
+			}
+		}
 	},
 
 	plugins: []
