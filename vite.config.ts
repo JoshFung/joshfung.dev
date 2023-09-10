da865c020eeced3ed3a70ec267b2be8be9adbe28
Josh Fung <joshuagyfung@gmail.com>
2023-09-09 19:56:16 -0700

    Changed output dir for build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
+	plugins: [sveltekit()],
 	server: {
 		fs: {
 			allow: [searchForWorkspaceRoot(process.cwd()), '/public']
 		}
 	},
-	plugins: [sveltekit()]
+	build: {
+		outDir: './build'
+	}
 });
