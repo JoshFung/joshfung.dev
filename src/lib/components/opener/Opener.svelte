2342f272d9fcc319a0c707dc00e59c31a1e7b21f
Josh Fung <joshuagyfung@gmail.com>
2023-09-09 18:38:34 -0700

    Pieced into smaller components

---
+++ src/lib/components/opener/Opener.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Title, Subheadings } from './index';
+</script>
+
+<div class="w-screen h-screen p-3 bg-rich-black">
+	<Title />
+	<Subheadings />
+</div>
+
+<style>
+</style>
