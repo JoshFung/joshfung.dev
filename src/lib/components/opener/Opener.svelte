4705ecd9103d9a9c475c673904ef44a63774f124
Josh Fung <joshuagyfung@gmail.com>
2023-09-09 19:03:37 -0700

    Spaced the subheadings and removed extra font

--- src/lib/components/opener/Opener.svelte
+++ src/lib/components/opener/Opener.svelte
@@ -2,7 +2,7 @@
 	import { Title, Subheadings } from './index';
 </script>
 
-<div class="w-screen h-screen p-3 bg-rich-black">
+<div class="w-screen h-screen p-3 bg-rich-black flex flex-col">
 	<Title />
 	<Subheadings />
 </div>
